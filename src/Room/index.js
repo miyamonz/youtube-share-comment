ccecb25845d6a039e34562608810d215d4b7e083
Koichi Miyamoto <m5813k@gmail.com>
2020-05-03 04:12:49 +0900

    fix layout

--- src/Room/index.js
+++ src/Room/index.js
@@ -14,16 +14,15 @@ export default function Room() {
   );
 }
 
+// ここにvideolistひっぱりだすべきかも
 function RoomLayout() {
   const {
     val: { chats = [] },
   } = useRoomContext();
   return (
     <>
-      <div style={{ display: "flex", height: 420 }}>
-        <div style={{ width: "100%" }}>
-          <VideoPlayer />
-        </div>
+      <div>
+        <VideoPlayer />
       </div>
     </>
   );
