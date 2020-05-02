7fbdf22b8c8d01e0ed4cb7c54d3fc4216efb7787
Koichi Miyamoto <m5813k@gmail.com>
2020-05-02 20:10:14 +0900

    move folder

--- src/Room/index.js
+++ src/Room/index.js
@@ -3,9 +3,7 @@ import React, { useState, useEffect } from "react";
 import { Provider, useRoomContext } from "./ContextRoom";
 import { useParams } from "react-router-dom";
 
-import VideoPlayer from "./VideoPlayer/index.js";
-import CommentArea from "./CommentArea";
-import ChatArea from "./ChatArea";
+import VideoPlayer from "../VideoPlayer/index.js";
 
 export default function Room() {
   const { name } = useParams();
