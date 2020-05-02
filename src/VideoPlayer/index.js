bbe322e4a25d3ed3d38094899c17ce2125bfd9e0
Koichi Miyamoto <m5813k@gmail.com>
2020-05-02 23:39:02 +0900

    動画 list

--- src/VideoPlayer/index.js
+++ src/VideoPlayer/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useRoomContext } from "../Room/ContextRoom";
+import { useList } from "react-firebase-hooks/database";
 
 import URLInput from "../URLInput";
 import VideoPlayer from "./VideoPlayer";
@@ -8,29 +9,64 @@ import CommentArea from "./CommentArea";
 import { Provider, useVideoContext } from "./VideoContext";
 
 export default function Container(props) {
-  const { ref: roomRef } = useRoomContext();
-  const videoRef = roomRef.child("video");
+  const { val, ref: roomRef } = useRoomContext();
+  const videoRef = roomRef.child("videos").orderByKey().ref;
+
+  const [snapshots] = useList(videoRef);
+
+  const currentVideoKey = val.currentVideoKey;
+  const [video, setVideo] = useState(() =>
+    currentVideoKey ? videoRef.child(currentVideoKey) : null
+  );
+  console.log(currentVideoKey, video);
+  const lastVideo = snapshots[snapshots.length - 1];
+
+  function sendVideoData(data) {
+    const dbRef = roomRef.child("videos").push();
+    dbRef.child("videoType").set(data);
+    dbRef.child("isPlaying").set(false);
+    dbRef.child("seekToTime").set(0);
+  }
+  function selectVideo(videoSnapshot) {
+    setVideo(videoSnapshot);
+    roomRef.child("currentVideoKey").set(videoSnapshot.key);
+  }
   return (
     <>
-      <Provider dbRef={videoRef}>
-        <InVideoContext />
-      </Provider>
+      <ul>
+        {snapshots.map((s) => {
+          return (
+            <li key={s.key}>
+              <a
+                onClick={(e) => {
+                  selectVideo(s);
+                  e.preventDefault();
+                }}
+                href="#"
+              >
+                {s.toJSON().videoType.id}
+              </a>
+              {s.key === video?.key && "←"}
+            </li>
+          );
+        })}
+      </ul>
+      <URLInput defaultVal={""} onChange={sendVideoData} />
+      {video && (
+        <Provider dbRef={video.ref}>
+          <InVideoContext />
+        </Provider>
+      )}
     </>
   );
 }
 function InVideoContext() {
   const {
     val: { videoType },
-    dbRef,
   } = useVideoContext();
   const url = videoType ? `https://youtube.com/?v=${videoType.id}` : "";
-  function sendVideoData(data) {
-    dbRef.child("videoType").set(data);
-    dbRef.child("isPlaying").set(false);
-  }
   return (
     <>
-      <URLInput defaultVal={url} onChange={sendVideoData} />
       {!videoType && "enter youtube URL"}
 
       {videoType && (
