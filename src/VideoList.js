b38b02b05d434001ab61583dbe8f39801f089edb
Koichi Miyamoto <m5813k@gmail.com>
2020-05-03 14:27:55 +0900

    戻す

--- src/VideoList.js
+++ src/VideoList.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 
 import { useList } from "react-firebase-hooks/database";
 
-import { getVideoTitle } from "./YoutubeAPI.js";
 import styled from "styled-components";
 
 function VideoList({ videosRef, currentVideoKey, onSelect }) {
@@ -12,13 +11,6 @@ function VideoList({ videosRef, currentVideoKey, onSelect }) {
     const isActive = snapshot.key === currentVideoKey;
 
     const videoId = snapshot.toJSON().videoType?.id;
-
-    const [title, setTitle] = useState(videoId);
-    useEffect(() => {
-      if (videoId) {
-        getVideoTitle(videoId).then(setTitle);
-      }
-    }, [videoId]);
     return (
       <a
         onClick={(e) => {
@@ -29,7 +21,7 @@ function VideoList({ videosRef, currentVideoKey, onSelect }) {
         className={isActive ? "is-active" : ""}
         {...props}
       >
-        {title}
+        {videoId}
       </a>
     );
   };
--- src/YoutubeAPI.js
+++ src/YoutubeAPI.js
@@ -1,9 +1,13 @@
 // this is availabe share-comment.web.app and my home ip address
 const key = "AIzaSyBDwf9EHvdy5ESV5Verx4wi8n-1ZgMtk9w";
 
-export function getVideoTitle(videoId) {
-  const URL = `https://www.googleapis.com/youtube/v3/videos?key=${key}&part=id,snippet&id=${videoId}`;
+export function getVideoTitle(videoIds) {
+  if (!Array.isArray(videoIds)) videoIds = [videoIds];
+  const URL = `https://www.googleapis.com/youtube/v3/videos?key=${key}&part=id,snippet&id=${videoIds.join(
+    ","
+  )}`;
   return fetch(URL)
     .then((res) => res.json())
-    .then((json) => json.items[0].snippet.title);
+    .then((json) => json.items)
+    .then((items) => items.map((i) => i.snippet.title));
 }
