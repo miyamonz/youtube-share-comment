e711dfa7b68d3f19a9501fde47283afe19a4b9e9
Koichi Miyamoto <m5813k@gmail.com>
2020-05-03 12:22:12 +0900

    move component and file

--- src/Room/index.js
+++ src/Room/index.js
@@ -4,6 +4,8 @@ import { Provider, useRoomContext } from "./ContextRoom";
 import { useParams } from "react-router-dom";
 
 import VideoPlayer from "../VideoPlayer/index.js";
+import VideoList from "../VideoList";
+import URLInput from "../URLInput";
 
 export default function Room() {
   const { name } = useParams();
@@ -17,13 +19,38 @@ export default function Room() {
 // ここにvideolistひっぱりだすべきかも
 function RoomLayout() {
   const {
-    val: { chats = [] },
+    val: { currentVideoKey },
+    ref: roomRef,
   } = useRoomContext();
+  const videosRef = roomRef.child("videos").orderByKey().ref;
+  const [video, setVideo] = useState();
+
+  function sendVideoData(data) {
+    const dbRef = roomRef.child("videos").push();
+    dbRef.child("videoType").set(data);
+    dbRef.child("isPlaying").set(false);
+    dbRef.child("seekToTime").set(0);
+  }
+  function selectVideo(videoSnapshot) {
+    roomRef.child("currentVideoKey").set(videoSnapshot.key);
+    setVideo(videoSnapshot);
+  }
   return (
-    <>
-      <div>
-        <VideoPlayer />
+    <div className="tile is-ancestor">
+      <div className="tile is-3">
+        <div>
+          <VideoList
+            {...{ videosRef, currentVideoKey }}
+            onSelect={selectVideo}
+          />
+          <URLInput defaultVal={""} onEnter={sendVideoData} />
+        </div>
       </div>
-    </>
+      <div className="tile">
+        <div className="tile is-child">
+          {video && <VideoPlayer video={video} />}
+        </div>
+      </div>
+    </div>
   );
 }
--- src/VideoPlayer/index.js
+++ src/VideoPlayer/index.js
@@ -1,53 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { useRoomContext } from "../Room/ContextRoom";
 
-import VideoList from "./VideoList";
-import URLInput from "../URLInput";
 
 import VideoPlayer from "./VideoPlayer";
 import CommentArea from "./CommentArea";
 
 import { Provider, useVideoContext } from "./VideoContext";
 
-export default function Container(props) {
-  const { val, ref: roomRef } = useRoomContext();
-
-  const videosRef = roomRef.child("videos").orderByKey().ref;
-  const currentVideoKey = val.currentVideoKey;
-
-  const [video, setVideo] = useState();
-
-  function sendVideoData(data) {
-    const dbRef = roomRef.child("videos").push();
-    dbRef.child("videoType").set(data);
-    dbRef.child("isPlaying").set(false);
-    dbRef.child("seekToTime").set(0);
-  }
-  function selectVideo(videoSnapshot) {
-    roomRef.child("currentVideoKey").set(videoSnapshot.key);
-    setVideo(videoSnapshot);
-  }
+export default function Container({ video }) {
   return (
-    <div className="tile is-ancestor">
-      <div className="tile is-3">
-        <div>
-          <VideoList
-            {...{ videosRef, currentVideoKey }}
-            onSelect={selectVideo}
-          />
-          <URLInput defaultVal={""} onEnter={sendVideoData} />
-        </div>
-      </div>
-      <div className="tile">
-        <div className="tile is-child">
-          {video && (
-            <Provider dbRef={video.ref}>
-              <InVideoContext />
-            </Provider>
-          )}
-        </div>
-      </div>
-    </div>
+    <Provider dbRef={video.ref}>
+      <InVideoContext />
+    </Provider>
   );
 }
 function InVideoContext() {
