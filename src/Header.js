313cdaf12187f86110f41c9beb5e7e1d2eee38c0
Koichi Miyamoto <m5813k@gmail.com>
2020-05-03 14:40:02 +0900

    fix navbar

--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Nav = styled.nav.attrs({ className: `navbar is-light` })``;
+const NavbarBrand = styled.div.attrs({ className: `navbar-brand` })``;
 const NavbarMenu = styled.div.attrs({ className: `navbar-menu` })``;
 const NavbarStart = styled.div.attrs({ className: `navbar-start` })``;
 const NavbarItem = styled.a.attrs({ className: `navbar-item` })``;
@@ -11,11 +12,10 @@ const NavbarLink = styled(Link).attrs({ className: `navbar-item` })``;
 function Header() {
   return (
     <Nav>
-      <NavbarMenu>
-        <NavbarStart>
-          <NavbarLink to="/">Home</NavbarLink>
-        </NavbarStart>
-      </NavbarMenu>
+      <NavbarBrand>
+        <NavbarLink to="/">Home</NavbarLink>
+      </NavbarBrand>
+      <NavbarMenu></NavbarMenu>
     </Nav>
   );
 }
